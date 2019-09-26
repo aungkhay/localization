aec2464c718f6d88eb6dbcbdac5d6e335f87b02d
Sai Aung Khay <saiaungkhaylamm@gmail.com>
2019-09-26 16:45:42 +0630

    refix

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('lang/en');
 });
 
 Route::group(['prefix' =>'/lang/{locale}', 'middleware'=>'lang'], function(){
